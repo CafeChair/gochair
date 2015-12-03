27d71087f5b1675153281ffedec7015a23acc3f1
orbzsh <orbzsh@orbzshdeMacBook-Pro.local>
2015-12-03 22:32:30 +0800

    modified:   README.md 	new file:   conf/app.conf 	modified:   models/models.go

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# x
+## GoChair
+####HttpDNS
+基于HTTP协议的DNS查询功能
+####RunCmd
+针对单独服务器的命令执行
+####Crontab
+定时任务执行
+####RunJob
+分布式的作业任务调度执行
+####Agent
+展示agnet的状态信息
--- models/models.go
+++ models/models.go
@@ -1,7 +1,29 @@
 package models
 
+import (
+	"gopkg.in/redis.v3"
+)
+
 type QueryStatus struct {
 	Status string
 	Domain string
 	Ipaddr string
+}
+
+type AgentStatus struct {
+	Uuid    string
+	Tag     string
+	Ipaddr  string
+	Version string
+	Status  string
+}
+
+func GetAgentStatus(redisaddr string) (*AgentStatus, error) {
+	agentstatus := new(AgentStatus)
+	redisClient := redis.NewClient(&redis.Options{Addr: redisaddr})
+	stucmd, err := redisClient.SMembers(agents).Result()
+	if err != nil {
+		return nil, err
+	}
+	return agentstatus, nil
 }
