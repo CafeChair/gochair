81c371b32c402f49b2a2f1a53c0c568920beeccf
orbzsh <orbzsh@orbzshdeMacBook-Pro.local>
2015-12-07 23:22:34 +0800

    modified:   conf/app.conf 	modified:   controllers/qdns.go 	new file:   gochair 	modified:   models/models.go

--- controllers/qdns.go
+++ controllers/qdns.go
@@ -2,11 +2,9 @@ package controllers
 
 import (
 	"fmt"
-	// "gochair/models"
-	"github.com/miekg/dns"
 	"github.com/astaxie/beego"
+	"gochair/models"
 )
-
 
 type QdnsController struct {
 	beego.Controller
@@ -18,27 +16,10 @@ func (self *QdnsController) Get() {
 }
 
 func (self *QdnsController) Post() {
-	domain:=self.Input().Get("domain")
-	dnss,err:= resolveDNS(domain)
-	if err !=nil {
+	domain := self.Input().Get("domain")
+	dnss, err := models.ResolveFromRedis(domain)
+	if err != nil {
 		self.Ctx.WriteString(fmt.Sprint(err))
 	}
 	self.Ctx.WriteString(fmt.Sprint(dnss))
-}
-
-func resolveDNS(domain string) ([]string,error) {
-	answer := make([]string,0)
-	m:= new(dns.Msg)
-	m.SetQuestion(dns.Fqdn(domain),dns.TypeA)
-	c := new(dns.Client)
-	in,_,err := c.Exchange(m,"119.29.29.29"+":53")
-	if err !=nil {
-		return answer,err
-	}
-	for _,ain := range in.Answer {
-		if a,ok := ain.(*dns.A);ok {
-			answer = append(answer,a.A.String())
-		}
-	}
-	return answer,nil
 }
--- models/models.go
+++ models/models.go
@@ -1,29 +1,52 @@
 package models
 
 import (
+	"github.com/astaxie/beego"
+	"github.com/miekg/dns"
 	"gopkg.in/redis.v3"
+	"strings"
+	"time"
 )
 
-type QueryStatus struct {
-	Status string
-	Domain string
-	Ipaddr string
-}
-
-type AgentStatus struct {
-	Uuid    string
-	Tag     string
-	Ipaddr  string
-	Version string
-	Status  string
-}
-
-func GetAgentStatus(redisaddr string) (*AgentStatus, error) {
-	agentstatus := new(AgentStatus)
-	redisClient := redis.NewClient(&redis.Options{Addr: redisaddr})
-	stucmd, err := redisClient.SMembers(agents).Result()
+func ResolveFromDNS(domain string) (string, error) {
+	answer := make([]string, 0)
+	m := new(dns.Msg)
+	m.SetQuestion(dns.Fqdn(domain), dns.TypeA)
+	c := new(dns.Client)
+	in, _, err := c.Exchange(m, beego.AppConfig.String("dns"))
 	if err != nil {
-		return nil, err
+		return strings.Join(answer, ","), err
 	}
-	return agentstatus, nil
+	for _, ain := range in.Answer {
+		if a, ok := ain.(*dns.A); ok {
+			answer = append(answer, a.A.String())
+		}
+	}
+	answerstr := strings.Join(answer, ",")
+	if err := cacheToRedis(domain, answerstr); err != nil {
+		return answerstr, nil
+	}
+	return strings.Join(answer, ","), err
+}
+
+func ResolveFromRedis(domain string) (string, error) {
+	redisClient := redis.NewClient(&redis.Options{Addr: beego.AppConfig.String("redis")})
+	strcmd, err := redisClient.Get(domain).Result()
+	if err == nil {
+		return strcmd, nil
+	}
+	str, errs := ResolveFromDNS(domain)
+	if errs == nil {
+		return str, errs
+	}
+	return str, errs
+}
+
+func cacheToRedis(domain, dnsstr string) error {
+	redisClient := redis.NewClient(&redis.Options{Addr: beego.AppConfig.String("redis")})
+	err := redisClient.Set(domain, dnsstr, time.Duration(60)*time.Second).Err()
+	if err != nil {
+		return err
+	}
+	return nil
 }
