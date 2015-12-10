7e8072fd8d00f83552e49422e14823265c093d57
orbzsh <orbzsh@orbzshdeMacBook-Pro.local>
2015-12-10 15:06:01 +0800

    modified:   agent.go

--- agent/agent.go
+++ agent/agent.go
@@ -2,15 +2,16 @@ package main
 
 //后续需要加个redis pool，避免过多的socket连接
 import (
+	"bytes"
+	"errors"
 	"flag"
 	"fmt"
 	"gochair/agent/x"
 	"gopkg.in/redis.v3"
 	"log"
-	"bytes"
-	"time"
 	"os/exec"
 	"strconv"
+	"time"
 )
 
 func Register() {
@@ -28,36 +29,47 @@ func AgentRun() {
 	//agent获取任务并执行
 	redisAddr := x.Config().Redis.Addr + ":" + strconv.Itoa(x.Config().Redis.Port)
 	redisClient := redis.NewClient(&redis.Options{Addr: redisAddr})
-	request, err := redisClient.HGet(x.Config().Uuid, "taskname").Result()
-	if err != nil {
-		log.Fatalln("agent get task fail: ", err)
+	for {
+		request, err := redisClient.HGet(x.Config().Uuid, "taskname").Result()
+		if err == redis.Nil {
+			time.Sleep(time.Second)
+			continue
+		}
+		if err != nil {
+			log.Fatalln("agent get task fail: ", err)
+			continue
+		}
+		_, err = redisClient.Del(x.Config().Uuid).Result()
+		if err != nil {
+			log.Fatalln("agent delete result fail: ", err)
+		}
+		result, err := ExecTask(request)
+		if err != nil {
+			log.Fatalln("agent run task fail: ", err)
+		}
+		fmt.Println(result)
 	}
-	result,err := ExecTask(request)
-	if err !=nil {
-		log.Fatalln("agent run task fail: ", err)
-	}
-	
 }
 
-func ExecTask(taskname string) (string,error) {
+func ExecTask(taskname string) (string, error) {
 	//执行任务命令
-	cmd := exec.Command("sh","-c", taskname)
+	cmd := exec.Command("sh", "-c", taskname)
 	var out bytes.Buffer
 	var stderr bytes.Buffer
 	cmd.Stdout = &out
 	cmd.Stderr = &stderr
 	cmd.Start()
 	cmdDone := make(chan error, 1)
-	go func(){
+	go func() {
 		cmdDone <- cmd.Wait()
 	}()
 
-	select{
-	case <- time.After(time.Duration(60) * time.Second):
+	select {
+	case <-time.After(time.Duration(60) * time.Second):
 		KillCmd(cmd)
-		<- cmdDone
+		<-cmdDone
 		return "", errors.New("Command timeout")
-	case err := <- cmdDone:
+	case err := <-cmdDone:
 		if err != nil {
 			log.Println(stderr.String())
 		}
@@ -71,13 +83,13 @@ func KillCmd(cmd *exec.Cmd) {
 	}
 }
 
-func LogToFile() bool {
-//任务执行日志保存到本地日志文件中	
-}
+// func LogToFile() bool {
+// 	//任务执行日志保存到本地日志文件中
+// }
 
-func LogToRedis() bool {
-//任务执行日志保存到redis中
-}
+// func LogToRedis() bool {
+// 	//任务执行日志保存到redis中
+// }
 
 func main() {
 	cfg := flag.String("c", "agent.json", "configfile")
