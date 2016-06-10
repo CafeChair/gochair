3618b1cbb1f991d9c86233903e36e84180e077e0
orbzsh <925511765@qq.com>
2016-06-11 01:41:17 +0800

    备份数据格式

--- gobackup/agent.go
+++ gobackup/agent.go
@@ -1,46 +1,47 @@
 /*
 1：/usr/local/agent/script/下的备份脚本备份完数据
-
 2：备份格式是（ipaddress_时间格式_数据类型-.tar.gz）
-
 3：agent把备份信息推送到redis队列中
-
 4：服务端从redis队列中取出并通过rsync拉取到异地备份机
-
 */
 
 package main
 
 import (
-	"bytes"
-	"encoding/json"
+	// "bytes"
+	// "encoding/json"
 	"fmt"
 	"github.com/Unknwon/goconfig"
 	"io/ioutil"
 	"net"
 	"os/exec"
 	"path/filepath"
+	"strings"
 	"time"
 )
 
 type BackupInfo struct {
-	ServerId        int32
-	GameId          int32
-	OpId            int32
-	ServerIp        string
-	Success         bool
+	ServerId        int64
+	Interval        int64 //上报时间间隔
+	GameId          int64
+	OpId            int64
+	ServerIp        string //本机的内网IP地址
+	Success         bool   //备份成功与否
 	BackupLog       string
-	BackupType      string
-	LastAllFilename string
-	Filename        string
-	Instance        int32
-	RsyncModel      string
-	FileSize        int32
-	CreateTime      *time.Time
-	LastAllTime     *time.Time
+	BackupType      string //备份的类型(MySQL or redis)
+	LastAllFilename string //上次全备的文件名称
+	Filename        string //备份的文件名称
+	Instance        int64  //数据库的端口
+	RsyncModel      string //rsync模块名称
+	FileSize        int64  //备份文件的大小
+	CreateTime      int64  //备份文件创建时间
+	LastAllTime     int64  //上次备份文件的创建时间
 }
 
 func FetchIpaddr() (string, error) {
+	/*
+		获取本机的内网IP地址
+	*/
 	var ipaddr string
 	addrs, err := net.InterfaceAddrs()
 	if err != nil {
@@ -75,14 +76,32 @@ func FetchAllScript(scirptPath string) ([]string, error) {
 	return scriptList, nil
 }
 
-func ExecuteScript(scriptName string) (*BackupInfo, error) {
+func FetchConfig(filename, section, key string) (string, error) {
+	/*
+		读取配置文件,并获取value值
+	*/
+	config, err := goconfig.LoadConfigFile(fileName)
+	if err != nil {
+		return "Error", err
+	}
+	value, err := config.GetValue(section, key)
+	if err != nil {
+		return "Error", err
+	}
+	return value, nil
+
+}
+func ExecuteScript(script string) ([]byte, error) {
 	/*
 		执行备份脚本
 	*/
-	cmd := exec.Command("sh", "-c", scriptName)
-	var stdout bytes.Buffer
-	cmd.Stdout = &stdout
-	cmd.Start()
+	command := strings.Split(script, " ")
+	cmd := exec.Command(command[0], command[1], command[2])
+	stdout, err := cmd.Output()
+	if err != nil {
+		return stdout, err
+	}
+	return stdout, nil
 }
 
 func main() {
--- gobackup/gobackup.md
+++ gobackup/gobackup.md
@@ -1,3 +1,41 @@
+##Gobackup
+####framework
+```
+
+```
+
+####agent config
+```
+[global]
+server_id = 39005900000000
+game_id = 39
+op_id = 590
+redis_host = 127.0.0.1
+redis_port = 6379
+redis_queue = msgqueue
+my_ip = 127.0.0.1
+log = /usr/local/agent/log/backup.log
+error_log = /usr/local/agent/log/backup_error.log
+
+[mysql3308]
+backup_type = mysql
+instance = 3308
+rsync_model = backup/database_3308
+back_dir = /data/backup/database_3308/
+back_log = /var/log/mysql_3308.log
+script = /bin/bash /usr/local/agent/script/mysql_backup.sh /etc/my.cnf
+
+[redis6381]
+backup_type = redis
+instance = 6381
+rsync_model = backup/redisbase_6381
+back_dir = /data/backup/redisbase_6381/
+back_log = /var/log/redis_6381.log
+script = /bin/bash /usr/local/agent/script/redis_backup.sh /etc/redis.conf
+```
+
+####Msg
+```
 {
     "server_id":39005900000000,
     "game_id":39,
@@ -14,4 +52,18 @@
     "file_size":477384,
     "create_time":1463496138
 }
+```
 
+####server
+```
+[global]
+rsync_bwlimit = 40960
+server_root_path = /backup/
+redis_host = 127.0.0.1
+redis_port = 6381
+redis_queue = msgqueue
+log = /usr/local/agent/log/back.log
+error_log = /usr/local/agent/log/back_error.log
+my_ip=127.0.0.1
+retry = 3
+```
